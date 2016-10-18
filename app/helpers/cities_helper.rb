128bc10c496620ad25459a34afd813eab4c59b0a
Eric Desjardins <eric.desjardins11@gmail.com>
2016-10-18 20:41:51 -0300

    Add Google Maps for Cities show

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -7,7 +7,6 @@ module CitiesHelper
     data = CSV.read('data/ca-cities-sample.csv', {:headers => true, :return_headers => true, :header_converters => :symbol, :converters => :all} )
 
     for i in 1..data.size-1 do
-      # puts "#{i} #{data[i][:name]}"
       @city = City.new(:name => data[i][:name], :province => data[i][:province], :latitude => data[i][:latitude], :longitude => data[i][:longitude])
       @city.save
     end
